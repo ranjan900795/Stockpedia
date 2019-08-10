e9ee2da8d378943dd7c19518cba9bd6d6397ee13
Shubham Ranjan <shubhamranjan542@gmail.com>
2019-08-11 01:19:39 +0530

    mobile view work started

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,41 +2,58 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{asset('https://fonts.googleapis.com/css?family=Crimson+Text&display=swap" rel="stylesheet')}}" rel="stylesheet">
     <link href="{{ asset('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('/css/dashboard.css')}}">
   </head>
-  <body class="bg-dark">
+  <body>
     <div class="container-fluid">
       <div class="row">
-        <nav class="navbar navbar-expand-lg  w-100 p-2">
-          <div class="col-3">
-            <p class="navbar-brand m-0 h4 text-white font-weight-bold">Hi! User</p>
-          </div>
-          <div class="col-9 d-flex justify-content-end">
-            <a class="text-muted h4 font-weight-bold" href="#">Logout</a>
-          </div>
-        </nav>
+        <div class="col-2  p-0 ">
+          <div class="pos-f-t">
+            <nav class="navbar navbar-dark ">
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+            </nav>
+            <div class="collapse" id="navbarToggleExternalContent">
+              <div class="col px-5">
+                <p class="navbar-brand m-0 h4 text-white font-weight-bold mx-1">Hi! User</p>
+              </div>
+              <div class=" p-4">
 
-        <div class="col-2  p-0 py-5">
-          <div class="col border-right ">
-            <div class="col  my-5">
-              <p class="text-muted h5 font-weight-bold">Portfolio<p>
-              <hr width="30%">
+                <div class="col border-right ">
+
+
+                  <div class="col  my-5">
+                    <p class="text-muted h5 font-weight-bold">Portfolio<p>
+                    <hr width="50%" class="navm">
+                  </div>
+                  <div class="col my-5">
+                    <p class="text-muted h5 font-weight-bold">Alloted Funds</p>
+                    <hr width="70%" class="navm">
+                  </div>
+                  <div class="col my-5">
+                    <p class="text-muted h5 font-weight-bold">Order Book</p>
+                    <hr width="60%" class="navm">
+                  </div>
+                  <div class="col my-5">
+                    <p class="text-muted h5 font-weight-bold">Transaction</p>
+                    <hr width="60%" class="navm">
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="col my-5">
-              <p class="text-muted h5 font-weight-bold">Alloted Funds</p>
-              <hr width="30%">
-            </div>
-            <div class="col my-5">
-              <p class="text-muted h5 font-weight-bold">Order Book</p>
-              <hr width="30%">
-            </div>
-            <div class="col my-5">
-              <p class="text-muted h5 font-weight-bold">Transaction</p>
-              <hr width="30%">
-            </div>
+
           </div>
+
         </div>
         <div class="col-10 p-0 ">
+          <nav class="navbar navbar-expand-lg  w-100 p-2">
+            <div class="col d-flex justify-content-end">
+              <a class="text-muted h4 font-weight-bold anchor" href="#">Logout</a>
+            </div>
+          </nav>
           <div class="col  px-5 pb-5">
             <p class="text-white h4">User's Alloted Funds</p>
           </div>
@@ -71,6 +88,8 @@
         </div>
       </div>
     </div>
+    <script type="text/javascript" src="{{ asset('https://code.jquery.com/jquery-3.3.1.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js') }}"></script>
   </body>
 </html>
