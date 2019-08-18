d5580089a2386f60eebb465084af7fcdfe6c9f7f
Subhajit Chatterjee <subhajitchatterjee425@gmail.com>
2019-08-18 15:06:59 +0530

    homepage with error in view-hight

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,52 +6,59 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link href= "{{asset('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css')}}" rel = stylesheet >
         <link href= "{{asset('css/style.css')}}" rel = stylesheet >
+        <link href= "{{asset('https://fonts.googleapis.com/css?family=Montserrat&display=swap')}}" rel = stylesheet>
         <title>Homepage</title>
     </head>
     <body>
         <div class="container-fluid m-0">
             <div class="row main">
-                <div class="col-lg-8 left border-right">
+                <div class="col-lg-8 left">
                     <div class="row logo">
-                        <h1>STOCK</h1>
-                        <h1>PEDIA</h1>
-                    </div>
-                    
-                    <div class="row content login">
-                        <div class="col login_signup">
-                            <a href="login.php">Login</a>
-                            <a href="registration.php">Registration</a>
+                        <div class="col p-0">
+                            <div class="row stock"><h1 class="border-bottom mb-0">STOCK</h1></div>
+                            <div class="row pedia"><h1 class=" pedia">PEDIA</h1></div>
                         </div>
-                        <div class="col content">
-                            <p>A simple initiation to make a virtual stock market platform where we can buy and sell stocks just like the real world.</p>
-                        </div>
+                        
                     </div>
+                    <div class="div row rotate">
+                        <div class="col-3">
+                            <div class="row">
+                                <div class="col-6 border-right ">
+                                    <a class="login_link" href="login.php">Login</a>
+                                </div>
+                                <div class="col-6 p-0 pl-4">
+                                    <a href="registration.php">Registration</a>
+                                </div>
+                            </div>
+                        </div>
 
-                    <div class="row footer">
-                        <p>Click here to see the available stocks in the market</p>
+                        <div class="col-9">
+                            <p>A simple initiation to make a virtual stock  market platform where we can buy and sell stocks just like the real world.</p>
+                        </div>                   
                     </div>
-                    
+                    <div class="row anchor mt-160px">
+                        <a class="ml-250px" href=""><p>click here to see all the available stocks</p></a>
+                    </div>
                 </div>
 
                 <div class="col-lg-4 right">
-                    <div class="row 1">
-                        <h5>What is this all about ?</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, facilis alias voluptatem quisquam expedita quo in minima vel omnis aliquam.</p>
-                    </div>
-                    <div class="row 2">
-                        <h5>How to play ?</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, facilis alias voluptatem quisquam expedita quo in minima vel omnis aliquam.</p>
-                    </div>
-                    <div class="row 3">
-                        
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, facilis alias voluptatem quisquam expedita quo in minima vel omnis aliquam.</p>
-                    </div>
-                    <div class="row 4">
-                        
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, facilis alias voluptatem quisquam expedita quo in minima vel omnis aliquam.</p>
+                    <div class="col howToPlay border-left">
+                        <div class="row 1 m-0">
+                            <h5>What is this all about ?</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, facilis alias voluptatem quisquam expedita quo in minima vel omnis aliquam.</p>
+                        </div>
+                        <div class="row 2 m-0">
+                            <h5>How to play ?</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, facilis alias voluptatem quisquam expedita quo in minima vel omnis aliquam.</p>
+                        </div>
+                        <div class="row 3 m-0">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, facilis alias voluptatem quisquam expedita quo in minima vel omnis aliquam.</p>
+                        </div>
+                        <div class="row 4 m-0">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, facilis alias voluptatem quisquam expedita quo in minima vel omnis aliquam.</p>
+                        </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </body>
